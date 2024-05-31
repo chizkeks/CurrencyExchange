9680edd0204783222c1322f2e36938520f0762fa
Ilya <pavlov.ilya.y@gmail.com>
2024-05-31 21:03:43 +0300

    Added the ability to process a PATCH method

--- src/main/java/servlets/ExchangeRateServlet.java
+++ src/main/java/servlets/ExchangeRateServlet.java
@@ -1,8 +1,11 @@
 package servlets;
 
 import com.google.gson.Gson;
+import dao.CurrencyDAO;
+import dao.CurrencyDAOImplSQLite;
 import dao.ExchangeRateDAO;
 import dao.ExchangeRateDAOImplSQLite;
+import model.Currency;
 import model.ErrorMessage;
 import model.ExchangeRate;
 
@@ -11,8 +14,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.Optional;
 
 @WebServlet("/exchangeRate/*")
@@ -28,15 +30,60 @@ public class ExchangeRateServlet  extends HttpServlet {
 
             //for url http://localhost:8080/CurrencyExchange/currency/RUR pathElements = [, RUR]
             //that's why we pass pathElements[1] to the function
-            System.out.println(pathElements[1].substring(0, 3));
-            System.out.println(pathElements[1].substring(3));
-            Optional<ExchangeRate> rate = dao.getByCurrencyPair(pathElements[1].substring(0, 3), pathElements[1].substring(3));
+            Optional<ExchangeRate> rate = dao.getByCurrencyPairCode(pathElements[1].substring(0, 3), pathElements[1].substring(3));
             if(rate.isPresent()) {
                 pw.println(new Gson().toJson(rate.get()));
             } else {
                 resp.setStatus(404);
                 pw.println(new Gson().toJson(new ErrorMessage("Обменный курс для пары не найден")));;
             }
+            return;
+        }
+        resp.setStatus(400);
+        pw.println(new Gson().toJson(new ErrorMessage("Коды валют пары отсутствуют в адресе")));
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String method = req.getMethod();
+        if(!method.equals("PATCH"))
+            super.service(req, resp);
+
+        doPatch(req, resp);
+    }
+
+    protected void doPatch(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter pw = resp.getWriter();
+
+        String[] pathElements = req.getPathInfo().split("/");
+
+        if(pathElements.length == 2 && pathElements[1].length() == 6) {
+
+            //check required fields
+            BufferedReader br = new BufferedReader(new InputStreamReader(req.getInputStream()));
+            String data = br.readLine();
+            String newRate  = "";
+            if(newRate == null || newRate.isEmpty()) {
+                resp.setStatus(400);
+                pw.println(new Gson().toJson(new ErrorMessage("Отсутствует нужное поле формы")));
+                return;
+            }
+
+            CurrencyDAO curDao = new CurrencyDAOImplSQLite();
+            //for url http://localhost:8080/CurrencyExchange/currency/RUR pathElements = [, RUR]
+            //that's why we pass pathElements[1] to the function
+            Optional<Currency> baseCurrency = curDao.getByCode(pathElements[1].substring(0, 3));
+            Optional<Currency> targetCurrency = curDao.getByCode(pathElements[1].substring(3));
+            if(baseCurrency.isPresent() && targetCurrency.isPresent()) {
+                ExchangeRateDAO erDao = new ExchangeRateDAOImplSQLite();
+                boolean result = erDao.updateRateByCurrencyPairId(baseCurrency.get().getId(), baseCurrency.get().getId(), Double.parseDouble(newRate));
+                if(result) {
+                    pw.println(new Gson().toJson(erDao.getByCurrencyPairCode(baseCurrency.get().getCode(), targetCurrency.get().getCode())));
+                    return;
+                }
+            }
+            resp.setStatus(404);
+            pw.println(new Gson().toJson(new ErrorMessage("Валютная пара отсутствует в базе данных")));;
             return;
         }
         resp.setStatus(400);
