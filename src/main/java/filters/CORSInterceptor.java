6cc06d45f607381b5210774a8d9b6bec5a8fc706
Ilya <pavlov.ilya.y@gmail.com>
2024-05-31 20:58:13 +0300

    Method PATCH added to Access-Control-Allow-Methods

--- src/main/java/filters/CORSInterceptor.java
+++ src/main/java/filters/CORSInterceptor.java
@@ -25,7 +25,7 @@ public class CORSInterceptor implements Filter {
         HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Headers", "*");
-        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, POST, DELETE");
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, POST, DELETE, PATCH");
 
         //Set encoding of both request and response to UTF-8
         servletResponse.setContentType("application/json; charset = UTF-8");
