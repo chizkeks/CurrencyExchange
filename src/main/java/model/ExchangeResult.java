c25feeec4cfcb689e6c14b4a69318eff5c882df0
Ilya <pavlov.ilya.y@gmail.com>
2024-05-31 21:05:16 +0300

    Added servlet to process GET call to /exchange?from=BASE_CURRENCY_CODE&to=TARGET_CURRENCY_CODE&amount=

---
+++ src/main/java/model/ExchangeResult.java
@@ -0,0 +1,54 @@
+package model;
+
+public class ExchangeResult {
+    private Currency baseCurrency;
+    private Currency targetCurrency;
+    private double rate;
+    private double amount;
+    private double convertedAmount;
+
+    public ExchangeResult(Currency baseCurrency, Currency targetCurrency, double rate, double amount) {
+        this.baseCurrency = baseCurrency;
+        this.targetCurrency = targetCurrency;
+        this.rate = rate;
+        this.amount = amount;
+        this.convertedAmount = rate * amount;
+    }
+
+    public Currency getBaseCurrency() {
+        return baseCurrency;
+    }
+
+    public void setBaseCurrency(Currency baseCurrency) {
+        this.baseCurrency = baseCurrency;
+    }
+
+    public Currency getTargetCurrency() {
+        return targetCurrency;
+    }
+
+    public void setTargetCurrency(Currency targetCurrency) {
+        this.targetCurrency = targetCurrency;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public double getConvertedAmount() {
+        return convertedAmount;
+    }
+
+}
